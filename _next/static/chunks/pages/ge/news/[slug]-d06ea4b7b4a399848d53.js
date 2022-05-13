b84d9c54bd84446bde35b9519c4a737c6d844612
Insaf Almukhametov <insaf_13@mail.ru>
2022-05-13 15:18:10 +0300

    Updates

---
+++ _next/static/chunks/pages/ge/news/[slug]-d06ea4b7b4a399848d53.js
@@ -0,0 +1,1 @@
+_N_E=(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9973],{88530:function(n,_,u){"use strict";u.r(_),u.d(_,{__N_SSG:function(){return t}});var e=u(71592),t=!0;_.default=e.Z},83131:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/ge/news/[slug]",function(){return u(88530)}])}},0,[[83131,2272,9774,3662,4885,9351,1791,6212,3106,6274,1592]]]);
