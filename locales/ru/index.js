4f21ebcf1f5e2aafc9ab70f34b3ddabde236f76b
Insaf Almukhametov <insaf_13@mail.ru>
2022-05-13 13:25:28 +0300

    Updates

---
+++ locales/ru/index.js
@@ -0,0 +1,15 @@
+import common from "./common.json";
+import components from "./components.json";
+import form from "./form.json";
+import interactive from "./interactive.json";
+import popup from "./popup.json";
+import states from "./states.json";
+
+export default {
+	common,
+	components,
+	form,
+	interactive,
+	popup,
+	states,
+};
