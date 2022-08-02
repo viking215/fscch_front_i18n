6218571142d54efd7cec8647e81ea174cdac8af8
deploy <danilkinivanUA@gmail.com>
2022-08-02 02:55:43 +0000

    Updates

---
+++ _next/static/chunks/pages/news/[slug]-8299f33d5988491435b0.js
@@ -0,0 +1,1 @@
+_N_E=(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5376],{95360:function(n,_,u){"use strict";u.r(_),u.d(_,{__N_SSG:function(){return t}});var e=u(71592),t=!0;_.default=e.Z},84507:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/news/[slug]",function(){return u(95360)}])}},0,[[84507,2272,9774,3662,4885,2634,9351,1791,4526,3106,1513,1592]]]);
