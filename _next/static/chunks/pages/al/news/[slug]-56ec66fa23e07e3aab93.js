c6b43e99f053e32d980c892e28e3b53bcd78b373
deploy <danilkinivanUA@gmail.com>
2022-10-03 16:26:41 +0000

    Updates

--- _next/static/chunks/pages/al/news/[slug]-56ec66fa23e07e3aab93.js
+++
@@ -1,1 +0,0 @@
-_N_E=(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8741],{145:function(n,_,u){"use strict";u.r(_),u.d(_,{__N_SSG:function(){return t}});var e=u(71592),t=!0;_.default=e.Z},90639:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/al/news/[slug]",function(){return u(145)}])}},0,[[90639,2272,9774,3662,4885,2634,9351,1791,4526,3106,6526,1592]]]);
---
+++ _next/static/chunks/pages/ja/news/[slug]-a73d74af99d6e505c648.js
@@ -0,0 +1,1 @@
+_N_E=(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5161],{6816:function(n,_,u){"use strict";u.r(_),u.d(_,{__N_SSG:function(){return t}});var e=u(71592),t=!0;_.default=e.Z},52966:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/ja/news/[slug]",function(){return u(6816)}])}},0,[[52966,2272,9774,3662,4885,2634,9351,1791,4526,3106,6337,1592]]]);
